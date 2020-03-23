a670849b7349740d861e575444878edd376a38c7
1179850608@qq.com <1179850608@qq.com>
2020-03-23 11:47:15 +0800

    add readme.md

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # mymap
 
+## 项目介绍
+
+用 vue、echarts 编写的实时疫情地图
+
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,112 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div ref="main" id="main" style="width: 1000px;height:600px;"></div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import echarts from "echarts";
+import "echarts/map/js/china.js";
+import jsonp from "jsonp";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+const options = {
+  title: {
+    text: "疫情地图"
+  },
+  series: [
+    {
+      type: "map",
+      map: "china",
+      name: "确诊人数",
+      label: {
+        //控制地区汉子
+        show: true,
+        color: "#333",
+        fontSize: 12
+      },
+      itemStyle: {
+        //地图板块的颜色和边框
+        areaColor: "#eee",
+        borderColor: "#666"
+      },
+      zoom: 1.2,
+      emphasis: {
+        //鼠标划过区域样式
+        label: {
+          color: "white",
+          fontSize: 14
+        },
+        itemStyle: {
+          areaColor: "#f50",
+          borderColor: "#666"
+        }
+      },
+      data: []
+    }
+  ],
+  tooltip: {
+    trigger: "item"
+  },
+  toolbox: {
+    show: true,
+    orient: "vertical",
+    left: "right",
+    top: "center",
+    feature: {
+      dataView: { readOnly: false },
+      restore: {},
+      saveAsImage: {}
+    }
+  },
+  visualMap: [
+    {
+      type: "piecewise",
+      pieces: [
+        { min: 10000 }, // 不指定 max，表示 max 为无限大（Infinity）。
+        { min: 1000, max: 9999 },
+        { min: 100, max: 999 },
+        { min: 10, max: 99 },
+        { max: 10 } // 不指定 min，表示 min 为无限大（-Infinity）。
+      ],
+      color: ["red", "#f6efa6"]
+    }
+  ]
+};
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  },
+  mounted() {
+    this.setEcharts();
+    // this.getData();
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    setEcharts() {
+      this.myEcharts = echarts.init(this.$refs.main);
+
+      this.myEcharts.setOption(options);
+    },
+    getData() {
+      jsonp(
+        "https://interface.sina.cn/news/wap/fymap2020_data.d.json?_=2580892522427",
+        {},
+        (err, data) => {
+          if (!err) {
+            console.log(data);
+            let list = data.data.list.map(item => ({
+              name: item.name,
+              value: item.value
+            }));
+            options.series[0].data = list;
+            this.myEcharts.setOption(options);
+          }
+        }
+      );
+    }
+  }
+};
+</script>
+<style scoped></style>
